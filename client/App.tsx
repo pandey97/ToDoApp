b86fd72e7af4d7cf08c9b38456d6563f65c835b3
Abhishek Gupta <abhishek.gupta1@successive.tech>
2024-09-21 10:09:05 +0530

    reactatron , moduleresolver added

--- client/App.tsx
+++ client/App.tsx
@@ -2,6 +2,15 @@ import React from 'react'
 import Navigation from './src/route'
 import { Provider } from 'react-redux'
 import store from './src/redux/store/Store'
+import { LogBox } from 'react-native';
+
+LogBox.ignoreAllLogs();
+
+if (__DEV__) {
+  import("./ReactotronConfig").then(() =>
+    console.log("Reactotron Configured for RNTypescriptBoilerplate"),
+  );
+}
 
 const App = () => {
   return (
--- client/src/core/ApiConstants.ts
+++ client/src/core/ApiConstants.ts
@@ -2,7 +2,7 @@ const APIConstants = {
   axiosCallTimeout: 30000,
   axiosCallRetryCount: 2,
   axiosCallRetryTimeout: 5000,
-  BaseURL: "http://10.0.2.2:8080/api/",
+  BaseURL: "http://192.168.29.9:8080/api/",
   SetUser: "auth/register",
   GetUser: "auth/login",
   SendTodo: "home/put-todo",
